cd855377ddc53dde726de3dad520130c862fccf6
SBSkylight <shura.gladkikh.04@bk.ru>
2021-01-28 18:45:04 +0700

    а

---
+++ src/hi.kt
@@ -0,0 +1,11 @@
+// For1
+//fun main()
+//{
+//    println("Введите числа K и N, неравное нулю")
+//    print("K: "); var k = readLine()!!.toInt()
+//    print("N: "); var n = readLine()!!.toInt()
+//    for (i in 0..n)
+//    {
+//        println("Число K равно $k")
+//    }
+//}
